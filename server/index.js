bdcd3ea21e1488b5c6d37f8e2c786d481651f3e9
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-09-26 12:53:30 +0530

    ClientType ad Queries

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(
-  "./graphql",
+  "/graphql",
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,7 @@ const { projects, clients } = require("../sampleData");
 const {
   GraphQLObjectType,
   GraphQLSchema,
+  GraphQLList,
   GraphQLID,
   GraphQLString,
 } = require("graphql");
@@ -20,6 +21,12 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
